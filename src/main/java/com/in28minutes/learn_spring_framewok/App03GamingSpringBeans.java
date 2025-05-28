9020b4cf7d9cd6a44a9571a37e308e0f3c42476c
James Glover <james.glover7@outlook.com>
2025-05-28 14:07:58 +0100

    Section 1 Complete

---
+++ src/main/java/com/in28minutes/learn_spring_framewok/App03GamingSpringBeans.java
@@ -0,0 +1,24 @@
+package com.in28minutes.learn_spring_framewok;
+
+import com.in28minutes.learn_spring_framewok.game.GameRunner;
+import com.in28minutes.learn_spring_framewok.game.GamingConsole;
+import com.in28minutes.learn_spring_framewok.game.PacmanGame;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App03GamingSpringBeans {
+    public static void main(String[] args) {
+
+        try (var context = new AnnotationConfigApplicationContext(GamingConfiguration.class)) {
+            context.getBean(GamingConsole.class).up();
+            context.getBean(GameRunner.class).run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+////        var marioGame = new MarioGame();
+//        var game = new PacmanGame(); //1: Object Creation
+//        var gameRunner = new GameRunner(game); //2: Object Creation + Wiring of Dependencies
+//        // Game is a Dependency of GameRunner
+//        gameRunner.run();
+    }
+}
